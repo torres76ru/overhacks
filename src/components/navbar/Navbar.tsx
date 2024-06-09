e35a2303a8d99a4acfdcd631b2dc3fa99bbfe3e0
rostyk7 <rostyslav.haidukov@live.com>
2024-06-09 12:03:15 +0200

    tasks page layout

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import classes from "./Navbar.module.scss";
+import { Link } from 'react-router-dom';
+import classes from './Navbar.module.scss';
 
 const Navbar = () => {
   return (
@@ -61,6 +61,58 @@ const Navbar = () => {
             </svg>
 
             <span>People</span>
+          </Link>
+        </li>
+        <li>
+          <Link className={classes.navbar__link} to="/earn">
+            <svg
+              width="22"
+              height="23"
+              viewBox="0 0 22 23"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <ellipse
+                cx="8.25"
+                cy="15.381"
+                rx="8.25"
+                ry="7.28574"
+                fill="white"
+              />
+              <ellipse
+                cx="13.75"
+                cy="9.71433"
+                rx="8.25"
+                ry="7.28574"
+                fill="white"
+              />
+              <ellipse
+                cx="13.75"
+                cy="7.28574"
+                rx="8.25"
+                ry="7.28574"
+                fill="white"
+              />
+              <path
+                d="M20.9337 7.28566C20.9337 10.7626 17.7356 13.6119 13.7503 13.6119C9.76502 13.6119 6.56699 10.7626 6.56699 7.28566C6.56699 3.80877 9.76502 0.959448 13.7503 0.959448C17.7356 0.959448 20.9337 3.80877 20.9337 7.28566Z"
+                fill="#D9D9D9"
+                stroke="#3F3F3F"
+                stroke-width="0.3"
+              />
+              <path
+                d="M16.35 12.9524C16.35 16.8763 12.7416 20.0881 8.25 20.0881C3.75843 20.0881 0.15 16.8763 0.15 12.9524C0.15 9.02838 3.75843 5.81663 8.25 5.81663C12.7416 5.81663 16.35 9.02838 16.35 12.9524Z"
+                fill="white"
+                stroke="#3F3F3F"
+                stroke-width="0.3"
+              />
+              <path
+                d="M15.4337 12.9524C15.4337 16.4293 12.2356 19.2786 8.25033 19.2786C4.26502 19.2786 1.06699 16.4293 1.06699 12.9524C1.06699 9.47552 4.26502 6.6262 8.25033 6.6262C12.2356 6.6262 15.4337 9.47552 15.4337 12.9524Z"
+                fill="#D9D9D9"
+                stroke="#3F3F3F"
+                stroke-width="0.3"
+              />
+            </svg>
+            <span>Earn</span>
           </Link>
         </li>
         <li>
